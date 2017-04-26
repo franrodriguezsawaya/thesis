a5d4c630cb9d5efe97a112735a1c8007835f0b90
franrodriguezsawaya <francesca.rodriguezsawaya@gmail.com>
2017-04-25 20:58:16 -0400

    cleanup content

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # thesis
 my nyu itp thesis so far
+
+
+## to do
+
+https://github.com/higuma/web-audio-recorder-js
--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -3,61 +3,71 @@
 //by fran rodriguez-sawaya
 //v0.0.1
 
-//https://github.com/higuma/web-audio-recorder-js
-// console.log("hi from speech js");
+//var for storing p5S.SpeechRec object
+var myRec = new p5.SpeechRec();
+//continuous recognition
+myRec.continuous = true;
+//allow partial recognition (faster, less accurate)
+myRec.interimResults = true;
 
-var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
-myRec.continuous = true; // do continuous recognition
-myRec.interimResults = true; // allow partial recognition (faster, less accurate)
-
-var x, y;
-var dx, dy;
-var fileNum = 0;
-
-function setup(){
-
-
-
-	noCanvas();
-	noLoop();
-	myRec.onResult = parseResult; // recognition callback
-	myRec.start(); // start engine
-}
-
-
-function parseResult(){
-
-	if(recording === true){
-		var mostrecentword = myRec.resultString.split(' ').pop();
-		mySpeech += mostrecentword + " ";
-		console.log(mySpeech);
-	}
-}
-
+//variable for storing speech
 var mySpeech = "";
-var recording = false;
+//variable for state of recording
+var isRecording = false;
 
-function saveMySpeechToFile(){
-	var filename = "mySpeecHText" + fileNum;
-	var blob = new Blob([mySpeech], {type: "text/plain;charset=utf-8"});
-	saveAs(blob, filename+".txt");
-	fileNum ++;
+//variable for keeping count of text files generated
+var fileCounter = 0;
+
+//setup() function from p5.js library
+function setup() {
+
+  //setup without canvas and no loop
+  noCanvas();
+  noLoop();
+
+  //recognition callback
+  myRec.onResult = parseResult;
+  //start engine
+  myRec.start();
 }
 
+//function called back by myRec when onResult
+function parseResult() {
+  if (isRecording === true) {
+    var mostrecentword = myRec.resultString.split(' ').pop();
+    mySpeech += mostrecentword + " ";
+    console.log(mySpeech);
+  }
+}
 
-window.addEventListener("load",function(){
+//function for saving speech to file
+function saveMySpeechToFile() {
+  var filename = "mySpeecHText" + fileCounter;
+  var blob = new Blob([mySpeech], {
+    type: "text/plain;charset=utf-8"
+  });
+  saveAs(blob, filename + ".txt");
+  //update fileCounter
+  fileCounter++;
+}
 
-	var startBtn = document.getElementById("start");
-	startBtn.addEventListener("click",function(){
-		recording = true;
-	});
+//add event listener to loading of page
+window.addEventListener("load", function() {
 
-
+	//retrieve start and stop buttons from site
+  var startBtn = document.getElementById("start");
 	var stopBtn = document.getElementById("stop");
-	stopBtn.addEventListener("click",function(){
-		saveMySpeechToFile();
-		mySpeech = "";
-		recording = false;
-	});
 
+	//add event listener on click to start button
+  startBtn.addEventListener("click", function() {
+		//toggle the recording on
+    isRecording = true;
+  });
+
+	//add event listener on click to stop button
+  stopBtn.addEventListener("click", function() {
+    saveMySpeechToFile();
+    mySpeech = "";
+    isRecording = false;
+  });
 });
